f9d5842b8e0f269c21cc37172e13900b2f6f3edf
Jorge Johnson <jorge.johnson.code@gmail.com>
2021-02-17 20:02:12 -0500

    some changes + 1B challenge

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+mod patterns;
+use std::io;
+use std::env;
+use std::io::prelude::*;
+fn readline()->String { io::stdin().lock().lines().next().unwrap().unwrap() }
+fn pattern_count() {
+    let text = readline();
+    let pattern = readline();
+    println!("{}", patterns::pattern_count(&text, pattern));
+}
+fn frecuent_words() {
+    let text = readline();
+    let k = readline().parse::<usize>();
+    if !k.is_err() {
+        println!("{:?}", patterns::frecuent_words(text, k.unwrap()));
+    }
+}
+
+fn main(){
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 || args.len() > 2 {
+        println!("this version requires exactly one command line argument.");
+        return;
+    }
+    match &args[1] as &str {
+        "-c" => pattern_count(),
+        "-t" => frecuent_words(),
+        &_ => println!("command {} not recognized.",&args[1])
+    }
+}
---
+++ src/patterns.rs
@@ -0,0 +1,36 @@
+    //challenge 1A, Bioinformatics Compeau-Pevzner
+    pub fn pattern_count(text:&String,pttrn:String) -> usize {
+        if text.len() < pttrn.len() {return 0}
+        let mut c:usize = 0;
+        for i in 0..=text.len() - pttrn.len() {
+            c += (text[i..i+pttrn.len()] == pttrn) as usize;
+        }
+        c
+    }
+
+    //challenge 1B, Bioinformatics Compeau-Pevzner
+    use std::collections::HashSet;
+    pub fn frecuent_words(text:String,k:usize) -> HashSet<String> {
+        let mut frequent_patterns:HashSet<String> = HashSet::new();
+        let n = text.len() - k + 1;
+        let mut count = vec![0;n];
+        for i in 0..n {
+            count[i] = pattern_count(&text, text[i..i+k].to_string());
+        }
+        let max_count = count.iter().max().unwrap();
+        for i in 0..n {
+            if count[i] == *max_count {
+                frequent_patterns.insert(text[i..i+k].to_string());
+            }
+        }
+        return frequent_patterns
+    }
+/*
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+*/
