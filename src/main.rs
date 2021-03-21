6424f0cd03d227ca1e45893347276d3b34b68d14
Jorge Johnson <jorge.johnson.code@gmail.com>
2021-03-21 11:27:02 -0500

    add problems 1C, 1D, 1F, 1G y 1H

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,47 @@ fn readline()->String { io::stdin().lock().lines().next().unwrap().unwrap() }
 fn pattern_count() {
     let text = readline();
     let pattern = readline();
-    println!("{}", patterns::pattern_count(&text, pattern));
+    println!("{}", patterns::pattern_count(&text, &pattern));
 }
-fn frequent_words() {
+fn most_frequent_words() {
     let text = readline();
     let k = readline().parse::<usize>();
     if !k.is_err() {
-        println!("{:?}", patterns::frequent_words(text, k.unwrap()));
+        println!("{:?}", patterns::most_frequent_words(&text, k.unwrap()));
     }
+}
+fn reverse_complement() {
+    let text = readline();
+    println!("{:?}", patterns::reverse_complement(text));
+}
+fn pattern_matching() {
+    let text = readline();
+    let pattern = readline();
+    for pos in patterns::pattern_matching(&text,pattern).iter() {
+        print!("{} ", pos);
+    }
+    println!();
+}
+fn approximate_pattern_matching() {
+    let text = readline();
+    let pattern = readline();
+    let d = readline().parse::<u32>();
+    for pos in patterns::approximate_pattern_matching(&text,pattern,d.unwrap()).iter() {
+        print!("{} ", pos);
+    }
+    println!();
+}
+
+fn minimum_skew() {
+    let text = readline();
+    let minskews = patterns::minimum_skew(&text);
+    println!("{:?}", minskews);
+}
+fn hamming_distance() {
+    let s = readline();
+    let t = readline();
+    let distance = patterns::hamming_distance(&s, &t);
+    println!("{:?}", distance);
 }
 
 fn main(){
@@ -24,7 +57,13 @@ fn main(){
     }
     match &args[1] as &str {
         "-pc" => pattern_count(),
-        "-fw" => frequent_words(),
-        &_ => println!("command {} not recognized.",&args[1])
+        "-fw" => most_frequent_words(),
+        "-rc" => reverse_complement(),
+        "-pm" => pattern_matching(),
+        "-apm" => approximate_pattern_matching(),
+//        "-cf" => clump_finding(), 
+        "-ms" => minimum_skew(),
+        "-hd" => hamming_distance(),
+&_ => println!("command {} not recognized.",&args[1])
     }
 }
--- src/patterns.rs
+++ src/patterns.rs
@@ -5,11 +5,11 @@
     //Se cuenta cuántas veces está el patrón pttrn en la cadena text
     //
     //--------------------------------------------
-    pub fn pattern_count(text:&String,pttrn:String) -> usize {
+    pub fn pattern_count(text:&str,pttrn:&String) -> usize {
         if text.len() < pttrn.len() {return 0}
         let mut c:usize = 0;
         for i in 0..=text.len() - pttrn.len() {
-            c += (text[i..i+pttrn.len()] == pttrn) as usize;
+            c += (&text[i..i+pttrn.len()] == pttrn) as usize;
         }
         c
     }
@@ -23,12 +23,12 @@
     //
     //--------------------------------------------
     use std::collections::HashSet;
-    pub fn frequent_words(text:String,k:usize) -> HashSet<String> {
-        let mut frequent_patterns:HashSet<String> = HashSet::new();
+    pub fn most_frequent_words(text:&str,k:usize) -> (HashSet<String>,usize) {
+        let mut frequent_patterns = HashSet::<String>::new();
         let n = text.len() - k + 1;
         let mut count = vec![0;n];
         for i in 0..n {
-            count[i] = pattern_count(&text, text[i..i+k].to_string());
+            count[i] = pattern_count(&text, &text[i..i+k].to_string());
         }
         let max_count = count.iter().max().unwrap();
         for i in 0..n {
@@ -36,14 +36,107 @@
                 frequent_patterns.insert(text[i..i+k].to_string());
             }
         }
-        frequent_patterns
+        (frequent_patterns,*max_count)
     }
-/*
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+
+    //--------------------------------------------
+    //challenge 1C, Bioinformatics Compeau-Pevzner
+    //
+    //REVERSE COMPLEMENT
+    //Find the reverse complement of a DNA string
+    //
+    //--------------------------------------------
+    use std::collections::HashMap;
+    pub fn reverse_complement(text:String) -> String {
+        let mut nuc_comp:HashMap<char,char> = HashMap::new();
+        nuc_comp.insert('A','T');
+        nuc_comp.insert('C','G');
+        nuc_comp.insert('G','C');
+        nuc_comp.insert('T','A');
+        let mut rev = String::new();
+        for c in text.chars().rev() {
+            rev.push(*nuc_comp.get(&c).unwrap());
+        }
+        rev
     }
-}
-*/
+
+    //--------------------------------------------
+    //challenge 1D, Bioinformatics Compeau-Pevzner
+    //
+    //PATTERN MATCHING
+    //Find positions of all ocurrences of a pattern in a string
+    //
+    //--------------------------------------------
+    pub fn pattern_matching(pttrn:&str, text:String) -> Vec<usize> {
+        let mut positions:Vec<usize> = Vec::new();
+        if text.len() < pttrn.len() {return positions }
+        for i in 0..=text.len() - pttrn.len() {
+            if &text[i..i+pttrn.len()] == pttrn {
+                positions.push(i);
+            }
+        }
+        positions
+    }
+
+    //--------------------------------------------
+    //challenge 1F, Bioinformatics Compeau-Pevzner
+    //
+    //MINIMUM SKEW
+    //Find A position in a genome where a skew diagram attains a minimum
+    //
+    //--------------------------------------------
+
+    pub fn minimum_skew(text:&String) -> std::vec::Vec<usize> {
+        struct Skews {value:i32, loci:Vec::<usize>};
+        let mut minpos = Skews{value:i32::MAX,loci:vec![0]};
+        let mut skew:i32 = 0;
+        let seq = text.as_bytes();
+        for i in 0..seq.len() {
+            if seq[i] == b'C' { skew -= 1}
+            else if seq[i] == b'G' { skew += 1}
+            if skew < minpos.value {
+                minpos.value = skew;
+                minpos.loci.clear();
+                minpos.loci.push(i+1);
+            }
+            else if skew == minpos.value {
+                minpos.loci.push(i+1);
+            }
+        }
+        minpos.loci
+    }
+
+    //--------------------------------------------
+    //challenge 1G, Bioinformatics Compeau-Pevzner
+    //
+    // HAMMING DISTANCE
+    // Compute the Hamming distance between two DNA strings.
+    //
+    //--------------------------------------------
+
+    pub fn hamming_distance(s:&str,t:&str) -> u32 {
+        let mut dist = 0;
+        let ( sb, tb )= (s.as_bytes(), t.as_bytes());
+        for i in 0..sb.len() {
+            dist += (sb[i] != tb[i]) as u32;
+        }
+        dist
+    }
+
+    //--------------------------------------------
+    //challenge 1H, Bioinformatics Compeau-Pevzner
+    //
+    //APPROXIMATE PATTERN MATCHING
+    //Find all aprproximate occurrences of a pattern in a string, with mismatches
+    //
+    //--------------------------------------------
+    pub fn approximate_pattern_matching(pttrn:&str, text:String, mismatch_number:u32) -> Vec<usize> {
+        let mut positions:Vec<usize> = Vec::new();
+        if text.len() < pttrn.len() {return positions }
+        for i in 0..=text.len() - pttrn.len() {
+            if  hamming_distance(&text[i..i+pttrn.len()],pttrn) <= mismatch_number {
+                positions.push(i);
+            }
+        }
+        positions
+    }
