3ccd853d41f793a961b1df404d1913d70632f684
Jorge Johnson <jorge.johnson.code@gmail.com>
2021-04-01 14:17:32 -0500

    new challenges of chapter 1

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn pattern_count() {
     let pattern = readline();
     println!("{}", patterns::pattern_count(&text, &pattern));
 }
+
 fn most_frequent_words() {
     let text = readline();
     let k = readline().parse::<usize>();
@@ -15,9 +16,39 @@ fn most_frequent_words() {
         println!("{:?}", patterns::most_frequent_words(&text, k.unwrap()));
     }
 }
+
+fn find_frequent_word_with_missmatch_by_sorting() {
+    let text = readline();
+    let values = readline();
+    let split = values.split(' ').collect::<Vec<&str>>();
+    let k = split[0].parse::<usize>().unwrap();
+    let d = split[1].parse::<usize>().unwrap();
+    println!("{:?}", patterns::find_frequent_word_with_missmatch_by_sorting(&text, k, d));
+}
+
+fn find_frequent_word_with_missmatch_and_reverse_by_sorting() {
+    let text = readline();
+    let values = readline();
+    let split = values.split(' ').collect::<Vec<&str>>();
+    let k = split[0].parse::<usize>().unwrap();
+    let d = split[1].parse::<usize>().unwrap();
+    println!("{:?}", patterns::find_frequent_word_with_missmatch_and_reverse_by_sorting(&text, k, d));
+}
+
+
+fn find_frequent_word_with_missmatch() {
+    let text = readline();
+    let values = readline();
+    let split = values.split(' ').collect::<Vec<&str>>();
+    let k = split[0].parse::<usize>().unwrap();
+    let d = split[1].parse::<usize>().unwrap();
+    println!("{:?}", patterns::find_frequent_word_with_missmatch(&text, k, d));
+}
+
+
 fn reverse_complement() {
     let text = readline();
-    println!("{:?}", patterns::reverse_complement(text));
+    println!("{:?}", patterns::reverse_complement(&text));
 }
 fn pattern_matching() {
     let text = readline();
@@ -30,11 +61,22 @@ fn pattern_matching() {
 fn approximate_pattern_matching() {
     let text = readline();
     let pattern = readline();
-    let d = readline().parse::<u32>();
+    let d = readline().parse::<usize>();
     for pos in patterns::approximate_pattern_matching(&text,pattern,d.unwrap()).iter() {
         print!("{} ", pos);
     }
     println!();
+}
+
+fn clump_finding() {
+    let text = readline();
+    let arguments = readline();
+    let values = arguments.split(" ").collect::<Vec<&str>>();
+    let k_size =values[0].parse::<usize>().unwrap();
+    let lwin =values[1].parse::<usize>().unwrap();
+    let times =values[2].parse::<usize>().unwrap();
+    let clump_patters = patterns::clump_finding(&text, k_size, lwin, times);
+    println!("{:?}", clump_patters);
 }
 
 fn minimum_skew() {
@@ -48,7 +90,31 @@ fn hamming_distance() {
     let distance = patterns::hamming_distance(&s, &t);
     println!("{:?}", distance);
 }
-
+fn pattern_to_number() {
+    let pttrn = readline();
+    let num = patterns::pattern_to_number(&pttrn);
+    println!("{:?}", num);
+}
+fn neighbors() {
+    let pttrn = readline();
+    let d = readline().parse::<usize>().unwrap();
+    let pttrns = patterns::neighbors(&pttrn,d);
+    for p in pttrns {
+        println!("{}", p);
+    }
+}
+fn number_to_pattern() {
+    let number = readline().parse::<usize>().unwrap();
+    let k = readline().parse::<usize>().unwrap();
+    let pttrn = patterns::number_to_pattern(number,k);
+    println!("{:?}", pttrn);
+}
+pub fn computing_frequences() {
+    let text = readline();
+    let k = readline().parse::<usize>().unwrap();
+    let frequences = patterns::computing_frequences(&text, k);
+    println!("{:?}", frequences);
+} 
 fn main(){
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 || args.len() > 2 {
@@ -57,13 +123,20 @@ fn main(){
     }
     match &args[1] as &str {
         "-pc" => pattern_count(),
+        "-ptn" => pattern_to_number(),
+        "-ntp" => number_to_pattern(),
         "-fw" => most_frequent_words(),
+        "-fwm" => find_frequent_word_with_missmatch(),
+        "-fwms" => find_frequent_word_with_missmatch_by_sorting(),
+        "-fwmrs" => find_frequent_word_with_missmatch_and_reverse_by_sorting(),
         "-rc" => reverse_complement(),
         "-pm" => pattern_matching(),
         "-apm" => approximate_pattern_matching(),
-//        "-cf" => clump_finding(), 
+        "-clf" => clump_finding(), 
         "-ms" => minimum_skew(),
         "-hd" => hamming_distance(),
-&_ => println!("command {} not recognized.",&args[1])
+        "-cf" => computing_frequences(),
+        "-n" => neighbors(),
+        &_ => println!("command {} not recognized.",&args[1])
     }
 }
--- src/patterns.rs
+++ src/patterns.rs
@@ -14,7 +14,6 @@
         c
     }
 
-
     //--------------------------------------------
     //challenge 1B, Bioinformatics Compeau-Pevzner
     //
@@ -47,7 +46,7 @@
     //
     //--------------------------------------------
     use std::collections::HashMap;
-    pub fn reverse_complement(text:String) -> String {
+    pub fn reverse_complement(text:&str) -> String {
         let mut nuc_comp:HashMap<char,char> = HashMap::new();
         nuc_comp.insert('A','T');
         nuc_comp.insert('C','G');
@@ -79,6 +78,46 @@
     }
 
     //--------------------------------------------
+    //challenge 1E, Bioinformatics Compeau-Pevzner
+    //
+    //CLUMP FINDING
+    //Find patterns forming clumps in a string
+    //This functions return All distinct patterns forming (L,t)-clups in text
+    //Based in improved version on page 46
+    //--------------------------------------------
+    pub fn clump_finding(genome:&str, k:usize, l:usize, t:usize) -> HashSet<String> {
+        let gnm:String = genome.to_string();
+        let mut patterns:HashSet<String> = HashSet::new();
+        let mut clump: Vec<u64> = vec![0; (4usize).pow(k as u32)];
+        let text = &genome[0..=l];
+        let mut frequency_array = computing_frequences(text, k);
+        for i in 0..(4usize).pow(k as u32)-1 {
+            if frequency_array[i] >= t {
+                clump[i] = 1;
+            }
+        }
+        for i in 1..gnm.len() - l {
+            let first_pattern = &genome[i-1..i-1+k];
+            let mut index = pattern_to_number(first_pattern);
+            frequency_array[index] -= 1;
+            let last_pattern = &genome[i+l-k..i+l];
+            index = pattern_to_number(last_pattern);
+            frequency_array[index] += 1;
+            if frequency_array[index] >= t {
+                clump[index] = 1;
+            }
+        }
+        for i in 0..(4usize).pow(k as u32) - 1 {
+            if clump[i] == 1 {
+                let pttrn = number_to_pattern(i,k);
+                patterns.insert(pttrn);
+            }
+        }
+        patterns
+    }
+
+
+    //--------------------------------------------
     //challenge 1F, Bioinformatics Compeau-Pevzner
     //
     //MINIMUM SKEW
@@ -87,7 +126,9 @@
     //--------------------------------------------
 
     pub fn minimum_skew(text:&String) -> std::vec::Vec<usize> {
-        struct Skews {value:i32, loci:Vec::<usize>};
+        struct Skews {
+            value:i32, loci:Vec::<usize>
+        }
         let mut minpos = Skews{value:i32::MAX,loci:vec![0]};
         let mut skew:i32 = 0;
         let seq = text.as_bytes();
@@ -114,11 +155,11 @@
     //
     //--------------------------------------------
 
-    pub fn hamming_distance(s:&str,t:&str) -> u32 {
-        let mut dist = 0;
+    pub fn hamming_distance(s:&str,t:&str) -> usize {
+        let mut dist = 0usize;
         let ( sb, tb )= (s.as_bytes(), t.as_bytes());
         for i in 0..sb.len() {
-            dist += (sb[i] != tb[i]) as u32;
+            dist += if sb[i] != tb[i] {1} else {0};
         }
         dist
     }
@@ -130,7 +171,7 @@
     //Find all aprproximate occurrences of a pattern in a string, with mismatches
     //
     //--------------------------------------------
-    pub fn approximate_pattern_matching(pttrn:&str, text:String, mismatch_number:u32) -> Vec<usize> {
+    pub fn approximate_pattern_matching(pttrn:&str, text:String, mismatch_number:usize) -> Vec<usize> {
         let mut positions:Vec<usize> = Vec::new();
         if text.len() < pttrn.len() {return positions }
         for i in 0..=text.len() - pttrn.len() {
@@ -139,4 +180,220 @@
             }
         }
         positions
+    }
+    pub fn approximate_pattern_count(text:&str, pttrn:&str, d:usize) -> usize {
+        let mut count = 0;
+        for i in 0..=text.len() - pttrn.len() {
+            if  hamming_distance(&text[i..i+pttrn.len()],&pttrn) <= d {
+                count += 1;
+            }
+        }
+        count
+    }
+
+    pub fn find_frequent_word_with_missmatch(text:&str,k:usize, d:usize) -> HashSet<String> {
+        let mut frequent = HashSet::<String>::new();
+        let mut frequency_array: Vec<usize> = vec![0; (4usize).pow(k as u32)];
+        let mut close: Vec<usize> = vec![0; (4usize).pow(k as u32)];
+        for i in 0..=text.len()-k {
+            let ngbhrs = neighbors(&text[i..i+k], d);
+            for nb in ngbhrs {
+                let index = pattern_to_number(&nb);
+                close[index] = 1;
+            }
+        }
+        for i in 0..(4usize).pow(k as u32) {
+            if close[i] == 1 {
+                let pttrn = number_to_pattern(i, k);
+                frequency_array[i] = approximate_pattern_count(text, &pttrn, d);
+            } 
+        }
+        let max_count:usize = *frequency_array.iter().max().unwrap();
+        for i in 0..(4usize).pow(k as u32) {
+            if frequency_array[i] == max_count {
+                let pttrn = number_to_pattern(i, k);
+                frequent.insert(pttrn);
+            }
+        }
+        frequent
+    }
+
+    //--------------------------------------------
+    //challenge 1I, Bioinformatics Compeau-Pevzner
+    //
+    //FREQUENT WORDS WITH MISSMATCH
+    //Se determina el k-mer de tamaño k más frecuente en el texto text
+    //
+    //--------------------------------------------
+    pub fn find_frequent_word_with_missmatch_by_sorting(text:&str,k:usize, d:usize) -> HashSet<String> {
+        let mut frequent = HashSet::<String>::new();
+        let mut neigborhoods = Vec::<String>::new();
+        for i in 0..=text.len()-k {
+            let ngbhrs = neighbors(&text[i..i+k], d);
+            for nb in ngbhrs {
+                neigborhoods.push(nb);
+            }
+        }
+        let mut index = vec![0;neigborhoods.len()];
+        let mut count = vec![1;neigborhoods.len()];
+        for i in 0..neigborhoods.len() {
+            index[i] = pattern_to_number(&neigborhoods[i]);
+        }
+        index.sort();
+        for i in 0..neigborhoods.len() - 1 {
+            if index[i] == index[i+1] {
+                count[i+1] = count[i] + 1;
+            }
+        }
+        let max_count:usize = *count.iter().max().unwrap();
+        for i in 0..neigborhoods.len()  {
+            if count[i] == max_count {
+                let pttrn = number_to_pattern(index[i], k);
+                frequent.insert(pttrn);
+            }
+        }
+        frequent
+    }
+
+    //--------------------------------------------
+    //challenge 1I, Bioinformatics Compeau-Pevzner
+    //
+    //FREQUENT WORDS WITH MISSMATCH
+    //Se determina el k-mer de tamaño k más frecuente en el texto text
+    //
+    //--------------------------------------------
+    pub fn find_frequent_word_with_missmatch_and_reverse_by_sorting(text:&str,k:usize, d:usize) -> HashSet<String> {
+        let mut frequent = HashSet::<String>::new();
+        let mut neigborhoods = Vec::<String>::new();
+        for i in 0..=text.len()-k {
+            let ngbhrs = neighbors(&text[i..i+k], d);
+            let rcomp = reverse_complement(&text[i..i+k]);
+            let rngbhrs = neighbors(&rcomp, d);
+            for nb in ngbhrs {
+                neigborhoods.push(nb);
+            }
+            for nb in rngbhrs {
+                neigborhoods.push(nb);
+            }
+        }
+        let mut index = vec![0;neigborhoods.len()];
+        let mut count = vec![1;neigborhoods.len()];
+        for i in 0..neigborhoods.len() {
+            index[i] = pattern_to_number(&neigborhoods[i]);
+        }
+        index.sort();
+        for i in 0..neigborhoods.len() - 1 {
+            if index[i] == index[i+1] {
+                count[i+1] = count[i] + 1;
+            }
+        }
+        let max_count:usize = *count.iter().max().unwrap();
+        for i in 0..neigborhoods.len()  {
+            if count[i] == max_count {
+                let pttrn = number_to_pattern(index[i], k);
+                frequent.insert(pttrn);
+            }
+        }
+        frequent
+    }
+    //--------------------------------------------
+    //challenge 1K, Bioinformatics Compeau-Pevzner
+    //
+    //COMPUTE FREQUENCIES
+    // generar un arreglo de frecuencias para los k-mers de una secuencia
+    //
+    //--------------------------------------------
+    pub fn computing_frequences(text:&str,k:usize) -> Vec<usize> {
+        let mut frequency_array: Vec<usize> = vec![0; (4usize).pow(k as u32)];
+        for i in 0..=text.len()-k as usize {
+            let pttrn = &text[i..i+k];
+            let j = pattern_to_number(pttrn) as usize;
+            frequency_array[j] += 1;
+        }
+        frequency_array
+    }  
+
+    //--------------------------------------------
+    //challenge 1L, Bioinformatics Compeau-Pevzner
+    //
+    //PATTERN TO NUMBER
+    //Se convierte un patrón de secuencia DNA a un número entero
+    //
+    //--------------------------------------------
+    fn sym_to_num(c:char)->usize { "ACGT".find(c).unwrap()}
+
+    fn prefix<'a>(p: &'a str) -> &'a str { 
+        if p.len() == 1 {
+            return &""
+        } 
+        return &p[0..p.len()-1]; 
+    }
+    fn last_symbol<'a>(p: &'a str) -> char  {p.chars().last().unwrap() }
+
+    pub fn pattern_to_number(pattern:&str)->usize {
+        if pattern.len() == 0 {return 0}
+        let pre = prefix(&pattern);
+        let sym = last_symbol(&pattern);
+        4 * pattern_to_number(pre) + sym_to_num(sym)
+    }
+
+    //--------------------------------------------
+    //challenge 1M, Bioinformatics Compeau-Pevzner
+    //
+    //NUMBER TO PATTERN
+    //Se convierte un número entero a un patrón de secuencia DNA
+    //
+    //--------------------------------------------
+    fn num_to_sym(n:usize)-> String { (['A','C','G','T'][n as usize]).to_string()}
+
+    pub fn number_to_pattern(index:usize,k:usize)->String {
+        if k == 1 {return num_to_sym(index)}
+        let q: usize = index / 4;
+        let r = index % 4;
+        let sym = num_to_sym(r);
+        let mut prefix_pattern = number_to_pattern(q, k - 1);
+        prefix_pattern.push_str(&sym);
+        prefix_pattern
+    }
+
+    //--------------------------------------------
+    //challenge 1N, Bioinformatics Compeau-Pevzner
+    //
+    //NEIGHBORS  
+    //The d-neighborhood Neighbors(Pattern, d) is the set of all k-mers 
+    //whose Hamming distance from Pattern does not exceed d.
+    //
+    //--------------------------------------------
+    pub fn neighbors<'a>(pattern:&str, d:usize) -> Vec<String> {
+        if d == 0 { 
+            let mut alone = Vec::<String>::new();
+            alone.push(String::from(pattern));
+            return alone
+        }
+        if pattern.len() == 1 {
+            let mut nucleotides = Vec::<String>::new();
+            nucleotides.push("A".to_string());
+            nucleotides.push("C".to_string());
+            nucleotides.push("G".to_string());
+            nucleotides.push("T".to_string());
+            return nucleotides
+        }
+        let mut neighborhood = Vec::<String>::new();
+        let suffix_neighbors = neighbors(&pattern[1..], d);
+        for text in suffix_neighbors {
+            if hamming_distance(&pattern[1..], &text) < d {
+                for &n in ["A","C","G","T"].iter() {
+                    let mut neigborh:String = String::new();
+                    neigborh.push_str(n);
+                    neigborh.push_str(&text);
+                    neighborhood.push(neigborh);
+                }
+            }
+            else {
+                let mut neigborh:String = String::from(&pattern[..=0]);
+                neigborh.push_str(&text);
+                neighborhood.push(neigborh);
+            }
+        }
+        neighborhood
     }
